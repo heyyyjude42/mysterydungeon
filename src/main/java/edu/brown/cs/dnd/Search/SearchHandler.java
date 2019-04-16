f8194a619857980010f8fe0bb75dd5cf4fbbb380
Jude <julie_wang1@brown.edu>
2019-04-15 20:20:07 -0400

    added roll functionality

--- src/main/java/edu/brown/cs/dnd/Search/SearchHandler.java
+++ src/main/java/edu/brown/cs/dnd/Search/SearchHandler.java
@@ -18,6 +18,8 @@ public class SearchHandler {
 
   public void registerCommands(CommandHandler handler) {
     handler.register("search", new SearchCommand());
+    handler.register("s", new SearchCommand());
+    handler.register("roll", new RollCommand());
   }
 
   private class SearchCommand implements Command {
@@ -28,6 +30,7 @@ public class SearchHandler {
       }
 
       // todo account for single search term / multiple words??
+      // todo: syntax: search "mage hand" vs search in spells "mage hand"
       String table = args[1];
       String term = args[2];
 
@@ -37,6 +40,46 @@ public class SearchHandler {
       } catch (SQLException e) {
         throw new CommandFailedException("ERROR: database search failed");
       }
+    }
+  }
+
+  private class RollCommand implements Command {
+    @Override
+    public void run(String[] args) throws CommandFailedException {
+      if (args.length != 2) {
+        throw new CommandFailedException("ERROR: expected syntax in the form " +
+            "of roll xdy, such as 4d6 or 1d8.");
+      }
+
+      String dice = args[1];
+
+      try {
+        System.out.println("Result: " + rollDice(dice));
+      } catch (NumberFormatException e) {
+        throw new CommandFailedException("ERROR: expected syntax in the form " +
+            "of roll xdy, such as 4d6 or 1d8.");
+      }
+    }
+
+    /**
+     * Takes in a string in the form of xdy and returns the corresponding
+     * roll amount. For instance, 2d8 will return a number between 2 and 16.
+     *
+     * @param dice String representation of dice to be rolled.
+     * @return The final roll in form of an int.
+     */
+    private int rollDice(String dice) {
+      String[] nums = dice.split("d");
+
+      int numDice = Integer.parseInt(nums[0]);
+      int diceFaces = Integer.parseInt(nums[1]);
+      int sum = 0;
+
+      for (int i = 0; i < numDice; i++) {
+        sum += Math.random() * diceFaces;
+      }
+
+      return sum;
     }
   }
 }
