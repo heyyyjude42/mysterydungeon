1aaac62dc28eb33099a9e93f458a31c9671dca61
Jude <julie_wang1@brown.edu>
2019-04-18 18:01:03 -0400

    added operators

---
+++ src/main/java/edu/brown/cs/dnd/Data/Comparator.java
@@ -0,0 +1,10 @@
+package edu.brown.cs.dnd.Data;
+
+public enum Comparator {
+  IS,
+  LESS_THAN,
+  GREATER_THAN,
+  LESS_THAN_OR_EQUALS,
+  GREATER_THAN_OR_EQUALS,
+  CONTAINS
+}
---
+++ src/main/java/edu/brown/cs/dnd/Data/SearchOperator.java
@@ -0,0 +1,37 @@
+package edu.brown.cs.dnd.Data;
+
+public class SearchOperator {
+  private Comparator comparator;
+  private String columnName;
+  private String term;
+  private boolean termIsString;
+
+  public SearchOperator(Comparator comparator, String columnName, String term
+      , boolean termIsString) {
+    this.comparator = comparator;
+    this.columnName = columnName;
+    this.term = term;
+    this.termIsString = termIsString;
+  }
+
+  @Override
+  public String toString() {
+    switch (comparator) {
+      case LESS_THAN:
+        return this.columnName + " < " + this.term;
+      case GREATER_THAN:
+        return this.columnName + " > " + this.term;
+      case LESS_THAN_OR_EQUALS:
+        return this.columnName + " <= " + this.term;
+      case GREATER_THAN_OR_EQUALS:
+        return this.columnName + " >= " + this.term;
+      case CONTAINS:
+        return "instr(" + this.columnName + ", \"" + this.term + "\") > 0";
+      case IS:
+        return this.columnName + " = " + (termIsString ? this.term :
+            "\"" + this.term + "\"");
+      default:
+        return "";
+    }
+  }
+}
