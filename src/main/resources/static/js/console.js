6ca8e469d84877cb0cf864f44be35815fc31d30c
Giuse Nguyen <giuse_nguyen@brown.edu>
2019-04-22 16:29:02 -0400

    added a delete function, next is adding from clicking the result list.

--- src/main/resources/static/js/console.js
+++ src/main/resources/static/js/console.js
@@ -27,17 +27,36 @@ $(document).ready(() => {
         console.log(output.length);
         if (resultIndex == 0) {
             console.log("Nothing to add.");
-        } else {
+        }  else {
             console.log("Storing result.");
+            //stickyTrack = resultIndex-1;
+            const removeTag  = $("<li>" + resultList[resultIndex - 1] + "</li>");
+            stickyNotes.append(removeTag);
+            let button = document.createElement("button");
+            button.innerHTML = "X";
+            //button.addEventListener("click", deleteEntry(removeTag));
+            //button.addEventListener("click", onclick());
+            button.addEventListener ("click", function() {
+                deleteEntry(removeTag);
+                button.remove();
+            });
+            stickyNotes.append(button);
+            
+            console.log(stickyNotes);
         }
-        stickyNotes.append("<p>" + resultList[resultIndex - 1] + "</p>");
+        //stickyNotes.append("<p>" + resultList[resultIndex - 1] + "</p>");
         console.log("appending:");
         console.log(resultList);
     });
 
-    //drawerHandle.addEventListener("click", onclick);
+    
 
 });
+
+function deleteEntry(removeTag){
+    console.log("Remove is triggered.")
+    removeTag.remove();
+}
 
 function query(line) {
     const postParameters = {input: line};
@@ -53,16 +72,17 @@ function query(line) {
         // if there's only one result, display the whole thing. Otherwise, display a shortened list.
         if (result.length === 1) {
             output.append("<p>" + prettified[0] + "</p>"); // TODO: make this custom for each data type later
+            resultList.push(prettified[0]);
+            resultIndex++;
         } else {
             for (let i = 0; i < prettified.length; i++) {
                 output.append("<div class='tooltip'>" + "<div class='displayText'>" + simplified[i] + "</div>" +
                     "<div class='right'><div class='tooltipText'>" + prettified[i] + "</div><i></i></div>" +
                     "</div></br>");
                 resultList.push(prettified[i]);
+                resultIndex++;
             }
-        }
-
-        resultIndex++;
+        }        
         // scroll to bottom
         output.scrollTop(output[0].scrollHeight);
     });
