7489d57e18878e60ccd3a44ba12012139878592b
Jude <julie_wang1@brown.edu>
2019-04-30 14:17:52 -0400

    added tiny woods

--- src/main/resources/static/js/dungeonGeneration.js
+++ src/main/resources/static/js/dungeonGeneration.js
@@ -112,7 +112,7 @@ function populateExistingTiles(terrain) {
         const tileStyleName = sheet.cssRules[i].selectorText.split(".")[1];
         let tileStyle = sheet.cssRules[i].cssText;
         tileStyle = tileStyle.split("{ ")[1].split(" }")[0];
-        tileStyle = tileStyle.replace("background:", "background: url('https://www.spriters-resource.com/resources/sheets/82/85430.png')");
+        tileStyle = tileStyle.replace("background:", "background: url('https://www.spriters-resource.com/resources/sheets/82/85135.png')");
         //tileStyle = tileStyle.replace("background:", "background: url('file://spritepacks\/" + terrain + "')");
         existingTileStyles[tileStyleName] = tileStyle;
     }
