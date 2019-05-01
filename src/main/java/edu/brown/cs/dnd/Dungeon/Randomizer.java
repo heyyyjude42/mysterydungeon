7e891eface0ff66853ca60d68642bf4ae7b723ef
Andrew Liu <andrewliu@andrews-mbp-2.devices.brown.edu>
2019-05-01 15:18:23 -0400

    Commiting to pull

--- src/main/java/edu/brown/cs/dnd/Dungeon/Randomizer.java
+++ src/main/java/edu/brown/cs/dnd/Dungeon/Randomizer.java
@@ -4,6 +4,14 @@ package edu.brown.cs.dnd.Dungeon;
  * Class representing a utility random number generator within a range.
  */
 public class Randomizer {
+
+  /**
+   * Method generates a random number between a specified min and max,
+   * inclusive.
+   * @param min   An int that is the min of the range
+   * @param max   An int that is the max of the range
+   * @return    An int that is the generated random number
+   */
   public static int generate(int min,int max)
   {
     return min + (int)(Math.random() * ((max - min) + 1));
